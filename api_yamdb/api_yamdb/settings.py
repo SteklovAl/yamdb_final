24ebff7fbc2d6b46c154c6231a990df80d895f4e
SteklovAl <stekaleksandr@yandex.ru>
2022-11-26 16:42:20 +0500

    final

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -5,12 +5,12 @@ from datetime import timedelta
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', '1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.33.31', 'localhost', 'stekaleksandr.sytes.net']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
